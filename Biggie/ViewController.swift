3146b3277905c89a22de1d3ca6cb2561fde0fabe
Nithish1990 <shannithish2990@gmail.com>
2023-05-24 11:13:54 +0530

    hello world

--- Biggie/ViewController.swift
+++ Biggie/ViewController.swift
@@ -13,7 +13,10 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        view.backgroundColor = .cyan
+        view.backgroundColor = .white
+        /*
+         ;afaksasmn;askdvnsdknvsd;pkv
+         */
     }
 
 
